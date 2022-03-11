87f2e17d446726fa317d8a1439dfa6ef13554e7f
ErlensGadzans  <erlens.gadzans@gmail.com>
2022-03-11 13:43:08 +0200

    Made navbar responsive

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//scroll function Addind & removing navbar background
 window.onscroll = function () {
   let nav = document.querySelector(".navbar");
   if (window.scrollY > 20) {
@@ -6,3 +7,13 @@ window.onscroll = function () {
     nav.classList.remove("sticky");
   }
 };
+
+//navbar menu responsive Button shows menu
+
+function responiveMenu() {
+  let element = document.querySelector(".navbar .menu");
+  element.classList.toggle("active");
+
+  let element2 = document.querySelector(".menu_btn i");
+  element2.classList.toggle("active");
+}
