5771e591d0a29970d2e8ea7ca18c4d5bde939005
ErlensGadzans  <erlens.gadzans@gmail.com>
2022-03-13 20:47:56 +0200

    Scroll to top btn added

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 //scroll function Addind & removing navbar background
 window.onscroll = function () {
   let nav = document.querySelector(".navbar");
+  let scroll = document.querySelector(".scroll_btn");
   if (window.scrollY > 20) {
     nav.classList.add("sticky");
   } else {
     nav.classList.remove("sticky");
   }
+  if (window.scrollY > 500) {
+    console.log(scroll);
+    scroll.classList.add("show");
+
+    document.querySelector(".scroll_btn").onclick = function () {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    };
+  } else {
+    scroll.classList.remove("show");
+  }
 };
 
 //navbar menu responsive Button shows menu
-
 function responiveMenu() {
   let element = document.querySelector(".navbar .menu");
   element.classList.toggle("active");
