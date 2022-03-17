2fec751dd83ad43823b15bff7537ef195c5006ad
ErlensGadzans  <erlens.gadzans@gmail.com>
2022-03-18 00:01:06 +0200

    .

--- ContactForm.php
+++ ContactForm.php
@@ -9,13 +9,13 @@ $message = $_POST["message"];
 $mailTo = "erlens.gadzans@gmail.com";
 $body = "";
 
-$body .= "You have a message from:" . $sendersName . "\r\n";
-$body .= "Senders e-mai:" . $sendersEmail . "\r\n";
-$body .= "Message:" . $message . "\r\n";
+// $body .= "You have a message from:" . $sendersName . "\r\n";
+// $body .= "Senders e-mai:" . $sendersEmail . "\r\n";
+// $body .= "Message:" . $message . "\r\n";
 
 
 
-mail($mailTo, $messageSubject, $body)
+mail($mailTo, $messageSubject, $message)
     or die("Error");
 
 echo "message send";
