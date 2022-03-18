48b96e1d87f41f767265a4b5e3900310e521fae5
ErlensGadzans  <erlens.gadzans@gmail.com>
2022-03-18 16:56:43 +0200

    Contact form finished & alert added

--- ContactForm.php
+++ ContactForm.php
@@ -1,16 +1,35 @@
 <?php
 
+if (isset($_POST["btn_send"])) {
 
-$sendersName = $_POST["name"];
-$sendersEmail = $_POST["email"];
-$messageSubject = $_POST["subject"];
-$message = $_POST["message"];
+    $sendersName = $_POST["name"];
+    $sendersEmail = $_POST["email"];
+    $messageSubject = $_POST["subject"];
+    $message = $_POST["message"];
 
-$mailTo = "erlens.gadzans@gmail.com";
+    $mailTo = "erlens.gadzans@gmail.com";
+    $body = "";
+
+    $body .= "You have an email from:" . $sendersName . "\r\n";
+    $body .= "Senders email:" . $sendersEmail . "\r\n";
+    $body .= "Message:" . $message . "\r\n";
+
+    // mail($mailTo, $messageSubject, $body);
+    // header('Location: index.php?success=true');
+    // or die("Error");
+    // $message_sent = true;
+    // header("Location: index.php?mailsended#contact");
+
+    mail($mailTo, $messageSubject, $body);
+    header("location:index.php?success#contact");
+}
 
 
 
-mail($mailTo, $messageSubject, $message)
-    or die("Error");
 
-echo "message send";
+
+// header("Location: index.html?mailsended#contact");
+
+// $alert = "<script>alert('lalala');</script>";
+// echo $alert;
+// header("Location: index.html?mailsended#contact");
